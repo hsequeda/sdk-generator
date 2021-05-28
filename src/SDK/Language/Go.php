89cc3cf22347d7d66fe7802855d401ae8ec84b98
Henry <henrysg88@gmail.com>
2021-05-28 13:34:20 -0400

    feat(Go): Add go.mod.twig template
    Added go.mod template. Added go module and go version params to Go language.

--- src/SDK/Language/Go.php
+++ src/SDK/Language/Go.php
@@ -4,226 +4,236 @@ namespace Appwrite\SDK\Language;
 
 use Appwrite\SDK\Language;
 
-class Go extends Language {
+class Go extends Language
+{
 
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return 'Go';
+  protected $params = [
+    'goModule' => 'github.com/appwrite/go-sdk',
+    'goVersion' => '1.14',
+  ];
+
+  /**
+   * @return string
+   */
+  public function getName()
+  {
+    return 'Go';
+  }
+
+  /**
+   * Get Language Keywords List
+   *
+   * @return array
+   */
+  public function getKeywords()
+  {
+    return [
+      'bool',
+      'error',
+      'for',
+      'func',
+      'go',
+      'if',
+      'import',
+      'make',
+      'map',
+      'nil',
+      'package',
+      'range',
+      'return',
+      'string',
+      'struct',
+      'type',
+      'var',
+    ];
+  }
+
+  /**
+   * @return array
+   */
+  public function getFiles()
+  {
+    return [
+      [
+        'scope'         => 'default',
+        'destination'   => 'main.go',
+        'template'      => 'go/main.go.twig',
+        'minify'        => false,
+      ],
+      [
+        'scope'         => 'default',
+        'destination'   => 'README.md',
+        'template'      => 'go/README.md.twig',
+        'minify'        => false,
+      ],
+      [
+        'scope'         => 'default',
+        'destination'   => 'CHANGELOG.md',
+        'template'      => 'go/CHANGELOG.md.twig',
+        'minify'        => false,
+      ],
+      [
+        'scope'         => 'default',
+        'destination'   => 'LICENSE',
+        'template'      => 'go/LICENSE.twig',
+        'minify'        => false,
+      ],
+      [
+        'scope'         => 'default',
+        'destination'   => 'client.go',
+        'template'      => 'go/client.go.twig',
+        'minify'        => false,
+      ],
+      [
+        'scope'         => 'default',
+        'destination'   => 'utils.go',
+        'template'      => 'go/utils.go.twig',
+        'minify'        => false,
+      ],
+      [
+        'scope'         => 'service',
+        'destination'   => '{{service.name | caseDash}}.go',
+        'template'      => 'go/services/service.go.twig',
+        'minify'        => false,
+      ],
+      [
+        'scope'         => 'method',
+        'destination'   => 'docs/examples/{{service.name | caseLower}}/{{method.name | caseDash}}.md',
+        'template'      => 'go/docs/example.md.twig',
+        'minify'        => false,
+      ],
+      [
+        'scope'         => 'default',
+        'destination'   => 'go.mod',
+        'template'      => 'go/go.mod.twig',
+        'minify'        => false,
+      ],
+    ];
+  }
+
+  /**
+   * @param $type
+   * @return string
+   */
+  public function getTypeName($type)
+  {
+    switch ($type) {
+      case self::TYPE_INTEGER:
+        return 'int';
+        break;
+      case self::TYPE_STRING:
+        return 'string';
+        break;
+      case self::TYPE_FILE:
+        return 'string'; // '*os.File';
+        break;
+      case self::TYPE_BOOLEAN:
+        return 'bool';
+        break;
+      case self::TYPE_ARRAY:
+        return '[]interface{}';
+        break;
     }
 
-    /**
-     * Get Language Keywords List
-     *
-     * @return array
-     */
-    public function getKeywords()
-    {
-        return [
-            'bool',
-            'error',
-            'for',
-            'func',
-            'go',
-            'if',
-            'import',
-            'make',
-            'map',
-            'nil',
-            'package',
-            'range',
-            'return',
-            'string',
-            'struct',
-            'type',
-            'var',
-        ];
+    return $type;
+  }
+
+  /**
+   * @param array $param
+   * @return string
+   */
+  public function getParamDefault(array $param)
+  {
+    $type       = $param['type'] ?? '';
+    $default    = $param['default'] ?? '';
+    $required   = $param['required'] ?? '';
+
+    if ($required) {
+      return '';
     }
 
-    /**
-     * @return array
-     */
-    public function getFiles()
-    {
-        return [
-            [
-                'scope'         => 'default',
-                'destination'   => 'main.go',
-                'template'      => 'go/main.go.twig',
-                'minify'        => false,
-            ],
-            [
-                'scope'         => 'default',
-                'destination'   => 'README.md',
-                'template'      => 'go/README.md.twig',
-                'minify'        => false,
-            ],
-            [
-                'scope'         => 'default',
-                'destination'   => 'CHANGELOG.md',
-                'template'      => 'go/CHANGELOG.md.twig',
-                'minify'        => false,
-            ],
-            [
-                'scope'         => 'default',
-                'destination'   => 'LICENSE',
-                'template'      => 'go/LICENSE.twig',
-                'minify'        => false,
-            ],
-            [
-                'scope'         => 'default',
-                'destination'   => 'client.go',
-                'template'      => 'go/client.go.twig',
-                'minify'        => false,
-            ],
-            [
-                'scope'         => 'default',
-                'destination'   => 'utils.go',
-                'template'      => 'go/utils.go.twig',
-                'minify'        => false,
-            ],
-            [
-                'scope'         => 'service',
-                'destination'   => '{{service.name | caseDash}}.go',
-                'template'      => 'go/services/service.go.twig',
-                'minify'        => false,
-            ],
-            [
-                'scope'         => 'method',
-                'destination'   => 'docs/examples/{{service.name | caseLower}}/{{method.name | caseDash}}.md',
-                'template'      => 'go/docs/example.md.twig',
-                'minify'        => false,
-            ],
-        ];
+    $output = ' = ';
+
+    if (empty($default) && $default !== 0 && $default !== false) {
+      switch ($type) {
+        case self::TYPE_NUMBER:
+        case self::TYPE_INTEGER:
+        case self::TYPE_BOOLEAN:
+          $output .= 'null';
+          break;
+        case self::TYPE_STRING:
+          $output .= '""';
+          break;
+        case self::TYPE_ARRAY:
+          $output .= '[]';
+          break;
+      }
+    } else {
+      switch ($type) {
+        case self::TYPE_NUMBER:
+        case self::TYPE_INTEGER:
+        case self::TYPE_ARRAY:
+          $output .= $default;
+          break;
+        case self::TYPE_BOOLEAN:
+          $output .= ($default) ? 'true' : 'false';
+          break;
+        case self::TYPE_STRING:
+          $output .= "\"{$default}\"";
+          break;
+      }
     }
 
-    /**
-     * @param $type
-     * @return string
-     */
-    public function getTypeName($type)
-    {
-        switch ($type) {
-            case self::TYPE_INTEGER:
-                return 'int';
-            break;
-            case self::TYPE_STRING:
-                return 'string';
-            break;
-            case self::TYPE_FILE:
-                return 'string'; // '*os.File';
-            break;
-            case self::TYPE_BOOLEAN:
-                return 'bool';
-            break;
-            case self::TYPE_ARRAY:
-                 return '[]interface{}';
-            break;
-        }
+    return $output;
+  }
 
-        return $type;
+  /**
+   * @param array $param
+   * @return string
+   */
+  public function getParamExample(array $param)
+  {
+    $type       = $param['type'] ?? '';
+    $example    = $param['example'] ?? '';
+
+    $output = '';
+
+    if (empty($example) && $example !== 0 && $example !== false) {
+      switch ($type) {
+        case self::TYPE_NUMBER:
+        case self::TYPE_INTEGER:
+        case self::TYPE_BOOLEAN:
+          $output .= 'null';
+          break;
+        case self::TYPE_STRING:
+          $output .= '""';
+          break;
+        case self::TYPE_ARRAY:
+          $output .= '[]';
+          break;
+        case self::TYPE_FILE:
+          $output .= "file";
+          break;
+      }
+    } else {
+      switch ($type) {
+        case self::TYPE_NUMBER:
+        case self::TYPE_INTEGER:
+        case self::TYPE_ARRAY:
+          $output .= $example;
+          break;
+        case self::TYPE_BOOLEAN:
+          $output .= ($example) ? 'true' : 'false';
+          break;
+        case self::TYPE_STRING:
+          $output .= "\"{$example}\"";
+          break;
+        case self::TYPE_FILE:
+          $output .= "file";
+          break;
+      }
     }
 
-    /**
-     * @param array $param
-     * @return string
-     */
-    public function getParamDefault(array $param)
-    {
-        $type       = $param['type'] ?? '';
-        $default    = $param['default'] ?? '';
-        $required   = $param['required'] ?? '';
-
-        if($required) {
-            return '';
-        }
-
-        $output = ' = ';
-
-        if(empty($default) && $default !== 0 && $default !== false) {
-            switch ($type) {
-                case self::TYPE_NUMBER:
-                case self::TYPE_INTEGER:
-                case self::TYPE_BOOLEAN:
-                    $output .= 'null';
-                    break;
-                case self::TYPE_STRING:
-                    $output .= '""';
-                    break;
-                case self::TYPE_ARRAY:
-                    $output .= '[]';
-                    break;
-            }
-        }
-        else {
-            switch ($type) {
-                case self::TYPE_NUMBER:
-                case self::TYPE_INTEGER:
-                case self::TYPE_ARRAY:
-                    $output .= $default;
-                    break;
-                case self::TYPE_BOOLEAN:
-                    $output .= ($default) ? 'true' : 'false';
-                    break;
-                case self::TYPE_STRING:
-                    $output .= "\"{$default}\"";
-                    break;
-            }
-        }
-
-        return $output;
-    }
-
-    /**
-     * @param array $param
-     * @return string
-     */
-    public function getParamExample(array $param)
-    {
-        $type       = $param['type'] ?? '';
-        $example    = $param['example'] ?? '';
-
-        $output = '';
-
-        if(empty($example) && $example !== 0 && $example !== false) {
-            switch ($type) {
-                case self::TYPE_NUMBER:
-                case self::TYPE_INTEGER:
-                case self::TYPE_BOOLEAN:
-                    $output .= 'null';
-                    break;
-                case self::TYPE_STRING:
-                    $output .= '""';
-                    break;
-                case self::TYPE_ARRAY:
-                    $output .= '[]';
-                    break;
-                case self::TYPE_FILE:
-                    $output .= "file";
-                    break;
-            }
-        }
-        else {
-            switch ($type) {
-                case self::TYPE_NUMBER:
-                case self::TYPE_INTEGER:
-                case self::TYPE_ARRAY:
-                    $output .= $example;
-                    break;
-                case self::TYPE_BOOLEAN:
-                    $output .= ($example) ? 'true' : 'false';
-                    break;
-                case self::TYPE_STRING:
-                    $output .= "\"{$example}\"";
-                    break;
-                case self::TYPE_FILE:
-                    $output .= "file";
-                    break;
-            }
-        }
-
-        return $output;
-    }
+    return $output;
+  }
 }
